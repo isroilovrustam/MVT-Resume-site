882eebab26682716aec7569def10a7b957c41db6
Rustam <rustamjonisroilov213@gmail.com>
2023-11-16 09:36:10 +0500

    admin

--- resume/admin.py
+++ resume/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from .models import About, Blog, Category, Tag, Comment, Collection, Contact, Newslatter
 
+
 # Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category')
+    list_filter = ('category', 'create_date')
+    search_fields = ('title',)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone')
+    list_filter = ('create_date',)
+
 
 admin.site.register(About)
-admin.site.register(Blog)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Comment)
-admin.site.register(Contact)
+
 admin.site.register(Collection)
 admin.site.register(Newslatter)
